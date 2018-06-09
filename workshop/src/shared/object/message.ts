1996e988ca174e341200feaf3190c73760208d6b
pmozola <mozola.pawel@gmail.com>
2018-06-09 12:05:56 +0200

    add messege class

---
+++ workshop/src/shared/object/message.ts
@@ -0,0 +1,15 @@
+export class Message {
+    body: string;
+    isRead: boolean;
+    receiver: string;
+    sender: string;
+    timestamp: Date;
+
+    constructor(body = '', isRead = false, receiver = '', sender = '') {
+        this.body = body;
+        this.isRead = isRead;
+        this.receiver = receiver;
+        this.sender = sender;
+        this.timestamp = new Date();
+    }
+}
